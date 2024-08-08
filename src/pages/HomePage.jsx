d5e09f4298c44995cf33ff7cba30cea8bb5dfc08
Cristian Camilo Garcia <cgarcia@caballo.com.co>
2024-08-08 15:01:41 -0500

    Se creao carpeta de imagenes y se agrego indicador al home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,12 @@ import Ranking from "../components/Ranking"
 
 const HomePage = () => {
   return (
-   <>
+    <>
     <Navbar/>
-    <h1 className="text-center font-bold mt-5">Eficiencas</h1>
-    <div className="container flex flex-wrap justify-around">
+    <div className="w-auto m-3">
     <Ranking/>
     </div>
+ 
    </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Permitir conexiones desde todas las interfaces de red
+    port: 3000,       // Puedes cambiar el puerto si es necesario
+  },
 })
